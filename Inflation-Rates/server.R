089816363aad42f3fb717e235f69b8466461d13c
Ansih Singh Walia <loveeanish@gmail.com>
2017-10-12 23:40:25 +0530

    added the plots to the dashboard

--- Inflation-Rates/server.R
+++ Inflation-Rates/server.R
@@ -1,17 +1,30 @@
 require(shinydashboard)
+require(ggplot2)
+require(dplyr)
 
 server <- function(input, output) { 
   
       output$tsplot<- renderPlot ({
         
-          if(input$country==inf$region){
+          
             
-            region<-filter(inf,region=input$country)#region is the dataframe of the country
+            df<-inf%>% filter(region==input$country)#making is the dataframe of the country
             
-            region$inflation<-as.numeric(region$inflation)
-            region$year<-as.numeric(region$year)
+            df$inflation<-as.numeric(df$inflation)
+            df$year<-as.numeric(df$year)
             
-          }
+            theme_set(theme_bw())
+            #plotting the time series plot
+            ggplot(aes(x=year,y=inflation),data=df) +
+              geom_point(size=2,color="orange") + 
+              geom_line(color="orange") +
+              scale_x_continuous(limits=c(1980,2017),breaks=seq(1980,2017,5)) 
+              #labs(x="Year",y="Inflation Rates",title="Time series of Inflation Rates for"+ input$country)
+            
+            
+            
+          
+
       
             })
   
