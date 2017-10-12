8c648039a8347b0dc0406579bfaf64e8cc9b67a1
Ansih Singh Walia <loveeanish@gmail.com>
2017-10-12 23:26:59 +0530

    making the server logic to plot the time series plots

--- Inflation-Rates/server.R
+++ Inflation-Rates/server.R
@@ -2,6 +2,18 @@ require(shinydashboard)
 
 server <- function(input, output) { 
   
+      output$tsplot<- renderPlot ({
+        
+          if(input$country==inf$region){
+            
+            region<-filter(inf,region=input$country)#region is the dataframe of the country
+            
+            region$inflation<-as.numeric(region$inflation)
+            region$year<-as.numeric(region$year)
+            
+          }
+      
+            })
   
   
     
