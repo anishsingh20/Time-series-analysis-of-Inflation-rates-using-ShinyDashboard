9b19dcd39738d9f39411c3015098444296d7ad2e
Ansih Singh Walia <loveeanish@gmail.com>
2017-10-12 22:41:41 +0530

    made the structure and layout of the dashboard

--- Inflation-Rates/server.R
+++ Inflation-Rates/server.R
@@ -1,1 +1,3 @@
 require(shinydashboard)
+
+server <- function(input, output) { }
--- Inflation-Rates/ui.R
+++ Inflation-Rates/ui.R
@@ -1,2 +1,17 @@
 library(shinydashboard)
 
+#layout of the dashboard
+
+dashboardPage(
+  #defines header
+  dashboardHeader(
+    dropdownMenu()
+  ),
+  #defines sidebar
+  dashboardSidebar(),
+  #defines bodys
+  dashboardBody(
+    
+    
+  )
+)
